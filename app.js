51375bf8936eb43531954665a00f26d4ccf2a8a6
danieldavid0901@gmail.com <60299227+Daniul@users.noreply.github.com>
2022-06-23 16:00:00 -0500

    added bootstrap to the front-end to make it look decent, thanks bootstrap

--- app.js
+++ app.js
@@ -1,73 +1,74 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const methodOverride = (require('method-override'));
-const campground = require('./models/campground');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const campground = require("./models/campground");
+const morgan = require("morgan");
+const ejsMate = require("ejs-mate");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
-
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console,"connection error:"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
-
 
 const app = express();
 
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/', (req,res) => {
-    res.render('home');
-})
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
 
-app.get('/campgrounds', async (req,res) => {
-    const campgrounds =  await campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-})
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
 
-app.get('/campgrounds/new', (req,res) => {
-    res.render('campgrounds/new'); 
-})
+app.post("/campgrounds", async (req, res) => {
+  const campgrounds = new campground(req.body.campground);
+  await campgrounds.save();
+  res.redirect(`/campgrounds/${campgrounds._id}`);
+});
 
-app.post('/campgrounds', async (req,res) => {
-    const campgrounds = new campground(req.body.campground);
-    await campgrounds.save();
-    res.redirect(`/campgrounds/${campgrounds._id}`);
-})
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const theCampground = await campground.findById(id);
+  res.render("campgrounds/show", { campground: theCampground });
+});
 
-app.get('/campgrounds/:id', async (req,res) => {
-    const {id} = req.params;
-    const theCampground = await campground.findById(id);
-    res.render('campgrounds/show',{campground: theCampground});
-})
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const theCampground = await campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground: theCampground });
+});
 
-app.get('/campgrounds/:id/edit',async(req,res) => {
-    const theCampground = await campground.findById(req.params.id);
-    res.render('campgrounds/edit',{campground: theCampground});
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  //Spread into object
+  const theCampground = await campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${theCampground._id}`);
+});
 
-})
-
-app.put('/campgrounds/:id', async (req,res) => {
-    const {id} = req.params;
-    //Spread into object
-    const theCampground = await campground.findByIdAndUpdate(id,{...req.body.campground});
-    res.redirect(`/campgrounds/${theCampground._id}`);
-})
-
-app.delete('/campgrounds/:id', async (req,res) => {
-    const {id} = req.params;
-    await campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-})
-
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
 
 app.listen(3000, () => {
-    console.log('Serving on Port 3000');
-})
+  console.log("Serving on Port 3000");
+});
--- seeds/index.js
+++ seeds/index.js
@@ -1,34 +1,37 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
-const campground = require('../models/campground');
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const campground = require("../models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
-
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console,"connection error:"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 //Picking random element from array:
 //array[Math.floor(Math.random() * array.length)]
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-
-const seedDB = async() => {
-    await campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
-    }
-}
+const seedDB = async () => {
+  await campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid esse molestiae praesentium neque, deleniti quisquam eligendi ipsam laboriosam tenetur exercitationem, dolorum odio, aut quam eaque magnam. Illo eligendi est rem?",
+      price: price,
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    db.close();
+  db.close();
 });
