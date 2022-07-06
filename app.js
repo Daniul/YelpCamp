c49c10262f49d2f781d8bb03d178712a065a9f9d
danieldavid0901@gmail.com <60299227+Daniul@users.noreply.github.com>
2022-07-05 21:32:06 -0500

    final push before deployment?

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
+const MongoDBStore = require("connect-mongo");
+// const dbUrl = process.env.DB_URL;
+
+//"mongodb://localhost:27017/yelp-camp"
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
@@ -41,7 +45,20 @@ app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
+const store = new MongoDBStore({
+  mongoUrl: "mongodb://localhost:27017/yelp-camp",
+  crypto: {
+    secret: "thisshouldbeabettersecret!",
+  },
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR!", e);
+});
+
 const sessionConfig = {
+  store,
   name: "session",
   secret: "thisshouldbeabettersecret",
   resave: false,
